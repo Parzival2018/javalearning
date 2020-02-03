7bef1003da0207f3cc158202178331fd85de439f
Parzival <1411843070@qq.com>
2020-02-03 12:09:16 +0800

    add RandomInit,Print,RanDomMess

--- 2020.2.3/src/MultidimensionalArrays.java
+++ 2020.2.3/src/MultidimensionalArrays.java
@@ -3,11 +3,14 @@ public class MultidimensionalArrays {
 
     public static void main(String[] args) {
         int[][] array = new int[5][6];
-        int[] x = {1, 2};
-        array[0] = x;
-        System.out.println(array[0][3]);
+        array=RandomInitArrays(5,6);
+        Print2DArrays(array);
+        array=randomPut(array);
+        Print2DArrays(array);
+
+
     }
-    int[][] randomPut(int[][] array)
+    static int[][] randomPut(int[][] array)
     {
         for(int row=0;row<array.length;row++)     //不加下标时使用.length求的是行数
             for(int column=0;column<array[row].length;column++)
@@ -20,6 +23,32 @@ public class MultidimensionalArrays {
             }
         return array;
     }
+    static int[][] RandomInitArrays(int row,int column)
+    {
+        int[][]array = new int[row][column];
+        for(int i=0;i<array.length;i++)
+        {
+            for(int j=0;j<array[i].length;j++)
+            {
+                array[i][j]=(int)(Math.random()*100);
+            }
+        }
+        return array;
+    }
+    static void Print2DArrays(int[][] array)
+    {
+        for(int i=0;i<array.length;i++)
+        {
+            for(int j=0;j<array[i].length;j++)
+            {
+                System.out.print(array[i][j]);
+                System.out.print(" ");
+            }
+
+        }
+        System.out.println("");
+    }
+
 }
 
 
